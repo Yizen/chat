b001672a5bea2a8c8d7e72c4ea48df430b06be53
Olivier Grenet <ogrenet@gmail.com>
2019-08-28 15:53:11 +0200

    Fix for filename

--- src/Services/MessageService.php
+++ src/Services/MessageService.php
@@ -123,7 +123,7 @@ class MessageService
             throw new \Exception('Message receiver has not been set');
         }
 
-        $command = new SendMessageCommand($this->to, $this->body, $this->from, $this->type);
+        $command = new SendMessageCommand($this->to, $this->body, $this->from, $this->type, $this->filename());
 
         return $this->commandBus->execute($command);
     }
